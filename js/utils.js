9633fa56772e8b701846a4bfd58a46e3a90800d0
Okami397 <aard.wolf@list.ru>
2023-04-17 22:00:13 +0600

    добавляет сортировку

--- js/utils.js
+++ js/utils.js
@@ -11,3 +11,25 @@ export const request = async (url, options) => {
   }
   return response.json();
 };
+
+/**
+ * @param {(...args: any) => any} callback
+ * @param {number} delay
+ * @returns {(...args: any) => any}
+ */
+export const debounce = (callback, delay = 500) => {
+  let timeoutId;
+  let lastCallTime;
+
+  return (...args) => {
+    const elapsedTime = Date.now() - lastCallTime;
+    const newDelay = Math.max(delay - elapsedTime, 0);
+
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      callback(...args);
+      lastCallTime = Date.now();
+    }, newDelay);
+  };
+};
